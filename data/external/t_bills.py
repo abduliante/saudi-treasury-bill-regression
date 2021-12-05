a4076aad0c81f4a9717296d7f549a095fc00d7d4
abdulium <aalrubaiya@gmail.com>
2021-12-05 23:06:10 +0300

    export t_bills csv

--- data/external/t_bills.py
+++ data/external/t_bills.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
-
+import csv
 
 driver = webdriver.Chrome('/usr/bin/chromedriver')
 driver.get('https://www.sama.gov.sa/en-US/GovtSecurity/pages/SAMABills.aspx?&&p_SAMABillYear=8_2020&p_SAMABillPeriod=20200901%2021%3a00%3a00&&PageFirstRow=1&View=4ee023c6-55dc-4522-bbc9-07e0458448c5')
@@ -36,12 +36,13 @@ for i in range(1,100):
 
     time.sleep(2)
 
+driver.close()
 
 
 # colnames
+colnames = []
 for i in sama_bill_table.find_all('tr')[0]:
-    print(i.text)
-
+    colnames.append(i.text)
 
 # for i in sama_bill_table.find_all('tr')[1:]:
 #     for j in i.find_all('td'):
@@ -69,4 +70,19 @@ date_yields_chop(bills)[0]
 # yields
 date_yields_chop(bills)[1]
 
-driver.close()
+len(date_yields_chop(bills)[1])
+
+
+def split_yields(l):
+    res = [l[i:i+5] for i in range(0, len(l), 5)]
+    return res
+
+
+with open('t_bills.csv', 'a') as f:
+    writer = csv.writer(f)
+    header = colnames
+    writer.writerow(header)
+    for i in range(len(date_yields_chop(bills)[0])):
+        week1, week4, week13, week26, week52 = split_yields(date_yields_chop(bills)[1])[i]
+        row = [date_yields_chop(bills)[0][i], week1, week4, week13, week26, week52]
+        writer.writerow(row)
